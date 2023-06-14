a0dbb9ca6a44cb0d410343b9efac59fc96c52b67
Alexander Parisi <parisialexander@gmail.com>
2023-06-14 15:43:00 -0400

    typo

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const devConfig = {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
-    host: process.env.PG_HPST,
+    host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
 }
