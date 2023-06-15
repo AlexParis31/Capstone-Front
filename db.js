6f273b128b79a226c8184944b8898e3a0cd94133
Alexander Parisi <parisialexander@gmail.com>
2023-06-15 18:09:46 -0400

    lo

--- db.js
+++ db.js
@@ -9,13 +9,15 @@ require("dotenv").config();
 //   port: process.env.PG_PORT,
 // };
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const devConfig = `postgresql://yiiturdymoiank:59f5ca120f930948224f326c75ef6c59b8d9c178542890b41c13609ddd8fa30c@ec2-54-156-8-21.compute-1.amazonaws.com:5432/$d2j3ghhqsg6981`;
 
 const proConfig = process.env.HEROKU_POSTGRESQL_PURPLE_URL; //heroku addons
 
 const pool = new Pool({
   connectionString:
-    proConfig 
+    process.env.NODE_ENV === "production" ? devConfig : proConfig,
 });
 
 module.exports = pool;
