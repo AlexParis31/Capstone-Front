0ea41074d23881b7bb6c1a5c6753b3662f51317a
Alexander Parisi <parisialexander@gmail.com>
2023-06-15 18:47:01 -0400

    k

--- db.js
+++ db.js
@@ -12,7 +12,10 @@ const devConfig = {
 
 
 const proConfig = {
-    connectionString: process.env.HEROKU_POSTGRESQL_PURPLE_URL //heroku addons
+    connectionString: process.env.HEROKU_POSTGRESQL_PURPLE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      } //heroku addons
 }
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
